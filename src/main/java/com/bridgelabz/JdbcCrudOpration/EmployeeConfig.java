c13b49aa994a22a39b0c1de311599a09803744ef
pranay <pranaykawale500@gmail.com>
2021-12-09 09:56:19 +0530

    Added:Welcome message added

---
+++ src/main/java/com/bridgelabz/JdbcCrudOpration/EmployeeConfig.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.JdbcCrudOpration;
+
+/**
+ * Hello world!
+ *
+ */
+public class EmployeeConfig 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/bridgelabz/JdbcCrudOpration/EmployeePayroll.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.JdbcCrudOpration;
+
+public class EmployeePayroll {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the JDBC CRUD Operation ");
+
+	}
+}
