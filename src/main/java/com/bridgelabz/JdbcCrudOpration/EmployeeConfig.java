0a7b13e12fb5ce310650561def44b03731be745c
pranay <pranaykawale500@gmail.com>
2021-12-09 10:03:35 +0530

    Added:Connecting java program to database

--- src/main/java/com/bridgelabz/JdbcCrudOpration/EmployeeConfig.java
+++ src/main/java/com/bridgelabz/JdbcCrudOpration/EmployeeConfig.java
@@ -1,13 +1,25 @@
 package com.bridgelabz.JdbcCrudOpration;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-/**
- * Hello world!
- *
- */
 public class EmployeeConfig 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+	 public Connection getConfig() {
+	        String dbURL = "jdbc:mysql://localhost:3306/employee_payroll_service";
+	        String uname = "root";
+	        String pass = "Pranay@1996";
+	        Connection connection = null;
+
+	        try {
+	            connection = DriverManager.getConnection(dbURL, uname, pass);
+	            if (connection != null) {
+	                System.out.println("Connected!");
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	        return connection;
+	    }
+	}
+	
--- src/main/java/com/bridgelabz/JdbcCrudOpration/EmployeePayroll.java
+++ src/main/java/com/bridgelabz/JdbcCrudOpration/EmployeePayroll.java
@@ -3,6 +3,7 @@ package com.bridgelabz.JdbcCrudOpration;
 public class EmployeePayroll {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the JDBC CRUD Operation ");
-
+		 EmployeeConfig eConfig = new EmployeeConfig();
+	        eConfig.getConfig();
 	}
 }
